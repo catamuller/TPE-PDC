eee5888ce3ef69864e7cf0a09d6dd513b7b28d2c
mquesada <mquesada@itba.edu.ar>
2024-06-21 22:34:17 -0300

    test+fix: simultaneous test and kmp license

---
+++ src/test/simultaneous_test.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <aio.h>
+#include <signal.h>
+
+#define TRUE 1
+
+#define MAX_PROCESSES 600
+#define ERROR -1
+#define SLAVE 0
+#define BUFFER_MAX_SIZE 524
+
+#define SLEEP_TIME 60
+
+int pids[MAX_PROCESSES] = {0};
+
+// NO HACER CTRL+C
+
+int main(void) {
+  for(int i=0;i<MAX_PROCESSES;i++) {
+    const struct timespec ts ={
+      .tv_nsec = 100000000,
+      .tv_sec  = 0
+    };
+    nanosleep(&ts,NULL);
+    pid_t forkPID = fork();
+    switch(forkPID) {
+      case ERROR:
+        perror("Fork error.");
+        exit(ERROR);
+      case SLAVE:
+        system("echo \"incomplete command to see ip and port number\r\n\" | nc -C localhost 2525 > /tmp/null");
+        while(TRUE);
+      default:
+        pids[i] = forkPID;
+    }
+  }
+  sleep(SLEEP_TIME);
+  for(int i=0;i<MAX_PROCESSES;i++) {
+    kill(pids[i], 9);
+  }
+}
